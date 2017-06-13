efaa1b9604474179f4c3cba4da94c1b02c111b82
RekGRpth <rekgrpth@gmail.com>
2017-06-13 09:35:54 +0500

    response

--- response.c
+++ response.c
@@ -21,23 +21,13 @@ int response_write(client_t *client, enum http_status code, char *body, int leng
     if ((error = client->tcp.type != UV_TCP)) { ERROR("client=%p, client->tcp.type=%i\n", client, client->tcp.type); return error; }
     if ((error = client->tcp.flags > MAX_FLAG)) { ERROR("client=%p, client->tcp.flags=%u\n", client, client->tcp.flags); return error; }
     if ((error = uv_is_closing((const uv_handle_t *)&client->tcp))) { ERROR("uv_is_closing\n"); return error; } // int uv_is_closing(const uv_handle_t* handle)
-    int headers_length = sizeof(HEADERS) - 1;
-    for (int number = length; number /= 10; headers_length++);
-    char headers[headers_length];
-    if ((error = snprintf(headers, headers_length, HEADERS, length) - headers_length + 1)) { ERROR("snprintf\n"); return error; }
-    int status_length = sizeof(STATUS) - 4;
-    const char *status_str = http_status_str(code);
-    status_length += strlen(status_str);
-    char status[status_length];
-    if ((error = snprintf(status, status_length, STATUS, client->parser.http_major, client->parser.http_minor, status_str) - status_length + 1)) { ERROR("snprintf:%s\n", status); return error; }
-    const uv_buf_t bufs[] = {
-        {.base = status, .len = status_length - 1},
-        {.base = headers, .len = headers_length - 1},
-        {.base = CRLF, .len = sizeof(CRLF) - 1},
-        {.base = body, .len = length}
-    };
     response_t *response = response_init();
     if ((error = !response)) { ERROR("response_init\n"); return error; }
+    if ((error = xbuf_xcat(&response->xbuf, STATUS, client->parser.http_major, client->parser.http_minor, http_status_str(code)) <= 0)) { ERROR("xbuf_xcat\n"); response_free(response); return error; }
+    if ((error = xbuf_xcat(&response->xbuf, HEADERS, length) <= 0)) { ERROR("xbuf_xcat\n"); response_free(response); return error; }
+    if ((error = xbuf_ncat(&response->xbuf, CRLF, sizeof(CRLF) - 1) <= 0)) { ERROR("xbuf_ncat\n"); response_free(response); return error; }
+    if ((error = xbuf_ncat(&response->xbuf, body, length) <= 0)) { ERROR("xbuf_ncat\n"); response_free(response); return error; }
+    const uv_buf_t bufs[] = {{.base = response->xbuf.base, .len = response->xbuf.len}};
     if ((error = !uv_is_writable((const uv_stream_t*)&client->tcp))) { ERROR("uv_is_writable\n"); response_free(response); client_close(client); return error; } // int uv_is_writable(const uv_stream_t* handle)
     if ((error = uv_write(&response->req, (uv_stream_t *)&client->tcp, bufs, sizeof(bufs) / sizeof(bufs[0]), response_on_write))) { ERROR("uv_write\n"); response_free(response); return error; } // int uv_write(uv_write_t* req, uv_stream_t* handle, const uv_buf_t bufs[], unsigned int nbufs, uv_write_cb cb)
     return error;
@@ -45,7 +35,6 @@ int response_write(client_t *client, enum http_status code, char *body, int leng
 
 static void response_on_write(uv_write_t *req, int status) { // void (*uv_write_cb)(uv_write_t* req, int status)
 //    DEBUG("req=%p, status=%i\n", req, status);
-//    DEBUG("ECANCELED=%i\n", -ECANCELED);
     if (status) ERROR("status=%i\n", status);
     client_t *client = (client_t *)req->handle->data;
     parser_init_or_client_close(client);
@@ -56,11 +45,13 @@ static void response_on_write(uv_write_t *req, int status) { // void (*uv_write_
 static response_t *response_init() {
     response_t *response = (response_t *)malloc(sizeof(response_t));
     if (!response) { ERROR("malloc\n"); return NULL; }
+    if (!xbuf_init(&response->xbuf)) { ERROR("xbuf_init\n"); response_free(response); return NULL; }
     response->req.data = (void *)response;
     return response;
 }
 
 static void response_free(response_t *response) {
 //    DEBUG("response=%p\n", response);
+    xbuf_free(&response->xbuf);
     free(response);
 }
--- response.h
+++ response.h
@@ -2,10 +2,12 @@
 #define _RESPONSE_H
 
 #include <uv.h> // uv_*
+#include "xbuffer.h" // xbuf_*
 #include "client.h"
 
 typedef struct response_t {
     uv_write_t req;
+    xbuf_t xbuf;
 } response_t;
 
 int response_write(client_t *client, enum http_status code, char *body, int length);
